1c731c57668edbd2506914a1849804a75562587a
Your Name <you@example.com>
2021-08-06 20:00:05 +0800

    bugfixed

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
-module.exports = {
 
+const styleImport  = require('vite-plugin-style-import');
+const eslintPlugin  = require('vite-plugin-eslint');
+module.exports = {
+    pluginOptions: {
+        vite: {
+            disabledTypeChecker: true,
+            plugins: [
+                styleImport.default({
+                    libs: [
+                      {
+                        libraryName: 'vant',
+                        esModule: true,
+                        resolveStyle: (name) => `vant/es/${name}/style`,
+                      },
+                    ],
+                  }),
+                  eslintPlugin.default({
+                    include: 'src/**/*.{vue,js,jsx,ts,tsx}',
+                    fix:true
+                  })
+            ]
+        }
+    }
 }
